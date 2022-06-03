9f090be20823d098d06b43268e6f89d2149cc704
VerseVent <mike.82pronka.27a@gmail.com>
2022-06-03 19:45:33 +0200

    create video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('play', function (e) {
+  console.log(e);
+});
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+function onTimeUpdate(e) {
+  console.log(e);
+  localStorage.setItem('videoplayer-current-time', String(e.seconds));
+}
